d291da801486c868dad3c079f7a88fa5a92617ee
Ryan Baermann <rcbaermann@gmail.com>
2019-07-24 10:40:38 -0700

    Cleaned up code

--- apps/users/views.py
+++ apps/users/views.py
@@ -11,52 +11,55 @@ def newUser(request):
     return render(request, 'users/user.html')
 
 def createUser(request):
-    #POST
     #Validate user data
-    #Create new user
-    #Redirect to user dashboard
     errors = User.objects.validateUser(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value)
         return redirect('/users/new')
 
-    newUser = User.objects.create(
-        firstName = request.POST['firstName'],
-        lastName = request.POST['lastName'],
-        email = request.POST['email'],
-        password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
-    )
-    newUser.save()
-    user = User.objects.get(email = request.POST['email'])
-    request.session['user'] = user.id
-    request.session['firstName'] = user.firstName
-    return redirect('/users/dashboard') 
+    #POST
+    if request.method == 'POST':
+
+        #Create new user
+        newUser = User.objects.create(
+            firstName = request.POST['firstName'],
+            lastName = request.POST['lastName'],
+            email = request.POST['email'],
+            password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        )
+        newUser.save()
+        user = User.objects.get(email = request.POST['email'])
+        request.session['user'] = user.id
+        request.session['firstName'] = user.firstName
+
+        #Redirect to user dashboard
+        return redirect('/users/dashboard') 
 
 def editUser(request, userId):
     #USER VALIDATION, WHO DO I WANT TO ALLOW ON THIS ROUTE?
-    #Get user
-    #Render the user edit page
     if request.session['user'] != int(userId):
         messages.error(request, 'You cannot edit someone else\'s page.')
         return redirect('/users/dashboard')
 
+    #Get user
     context = {
         'user' : User.objects.get(id = userId)
     }
+
+    #Render the user edit page
     return render(request, 'users/editUser.html', context)
 
 def updateUser(request, userId):
     #USER VALIDATION, WHO DO I WANT TO ALLOW ON THIS ROUTE?
-    #POST
-    #Validate data
-    #Update the user
-    #Redirect to view user
     if request.session['user'] != int(userId):
         messages.error(request, 'You can not edit someone else\'s page.')
         return redirect(f'/users/dashboard')
+
+    #POST
     if request.method == "POST":
 
+        #Validate data
         errors = User.objects.validateUserEdit(request.POST)
 
         if len(errors) > 0:
@@ -64,68 +67,79 @@ def updateUser(request, userId):
                 messages.error(request, value)
             return redirect(f'/users/{ userId }/edit')
 
-        else:
-            user = User.objects.get(id = userId)
-            user.firstName = request.POST['firstName'] or user.firstName
-            user.lastName = request.POST['lastName'] or user.lastName
-            user.email = request.POST['email'] or user.email
-            user.password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()) or user.password
-            user.save()
-            
+        #Update the user
+        user = User.objects.get(id = userId)
+        user.firstName = request.POST['firstName'] or user.firstName
+        user.lastName = request.POST['lastName'] or user.lastName
+        user.email = request.POST['email'] or user.email
+        user.password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()) or user.password
+        user.save()
+
+    #Redirect to view user
     return redirect('/users/dashboard')
 
 def deleteUser(request, userId):
     #USER VALIDATION, WHO DO I WANT TO ALLOW ON THIS ROUTE?
-    #Delete user from database
-    #Redirect to index
     if request.session['user'] != int(userId):
         messages.error(request, 'You can not delete someone else\'s page.')
         return redirect('/users/dashboard')
     
+    #Delete user from database
     delUser = User.objects.get(id = userId)
     delUser.delete()
+
+    #Redirect to index
     return redirect ('/')
 
 def login(request):
-    #POST
     #Validate user
-    #log user in (don't forget session)
-    #Redirect to dashboard
     errors = User.objects.validateLogin(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value)
         return redirect('/')
 
-    else:
+    #POST
+    if request.method == 'POST':
+
+        #log user in (don't forget session)
         user = User.objects.get(email = request.POST['email'])
         request.session['user'] = user.id
         request.session['firstName'] = user.firstName
+
+        #Redirect to dashboard
         return redirect('/users/dashboard')
 
 def logout(request):
     #Remove user from session
-    #Return to index
     request.session.clear()
+
+    #Return to index
     return redirect('/')
 
 def userDashboard(request):
     #USER VALIDATION, WHO DO I WANT TO ALLOW ON THIS ROUTE?
-    #Renders the main page for the user
     if 'user' not in request.session:
         messages.error(request, 'You must be logged in.')
         return redirect('/')
+
     user = User.objects.get(id = request.session['user'])
     context = {
         'user' : user
     }
+
+    #Renders the main page for the user
     return render(request, 'users/dashboard.html', context)
 
 def deleteLine(request, userId):
+    #USER VALIDATION, WHO DO I WANT TO ALLOW ON THIS ROUTE?
     if request.session['user'] != int(userId):
         messages.error(request, "You do not have authorization to do that")
         return redirect('/users/dashboard')
 
+    #Delete user from database
     delLine = LineMember.objects.get(member = userId)
     delLine.delete()
+
+    #Return to dashboard
     return redirect('/users/dashboard')
