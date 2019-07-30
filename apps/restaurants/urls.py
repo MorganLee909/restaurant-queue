8cef1492443f09a4b9b3b01ae892a25c546ffef8
Lee Morgan <Morgan.LeeR@protonmail.com>
2019-07-30 14:41:38 -0700

    Create modal to create temporary customer

--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     url(r'^tables/(?P<tableId>\d+)/assign$', views.assignTable),
     url(r'^removeParty/(?P<partyId>\d+)$', views.removeParty),
     url(r'^checkoutParty/(?P<partyId>\d+)$', views.checkout),
-    url(r'^adduser$', views.addTempUser),
     url(r'^createuser$', views.createTempUser)
 ]
